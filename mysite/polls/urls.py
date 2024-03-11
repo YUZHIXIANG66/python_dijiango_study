7e1f6a38c10d1a6528c9578396e2e5e6d5dd68d4
YuZhiXiang <yzx2109337235@gmail.com>
2024-03-11 17:07:25 +0800

    第2次提交：完成基本html标签--model_test.html。

--- mysite/polls/urls.py
+++ mysite/polls/urls.py
@@ -5,4 +5,7 @@ urlpatterns = [
     path("",views.index,name="index"),
     path("user_list",views.user_list),
     path("user_add",views.user_add),
+    path("model_test",views.model_test),
+    path("news",views.news),
+    path("user_register",views.user_register),
 ]
--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+import requests
 
 # Create your views here.
 from django.http import HttpResponse
@@ -10,3 +11,21 @@ def user_list(request):
 
 def user_add(request):
     return render(request,"user_add.html")
+
+def model_test(request):
+    name = "Zhao"
+    roles = ["管理","CEO","保安"]
+    user_info={"name":"YU","salary":1000,'role' : 'CEO'}
+    return render(request,"model_test.html",{"n1":name,"n2":roles,"user_info":user_info})
+def news(request):
+    #发请求：https://ldocctvwbcdali.v.myalicdn.com/ldocctvwbcd/cdrmldcctv4_1md.m3u8
+    res = requests.get("https://ldocctvwbcdali.v.myalicdn.com/ldocctvwbcd/cdrmldcctv4_1md")
+    print(res)
+    return render(request,'news.html')
+def user_register(request):
+    return render(request,"user_register.html")
+
+if __name__== '__main__':
+    res = requests.get("https://ldocctvwbcdali.v.myalicdn.com/ldocctvwbcd/cdrmldcctv4_1md")
+    print(res)
+
