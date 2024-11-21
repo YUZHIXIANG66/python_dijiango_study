2617bc38932b5266fc9ed88d9d547b6ca68d8a7e
YuZhiXiang <yzx2109337235@gmail.com>
2024-11-21 16:09:54 +0800

    第4次提交：开始搭建毕设网页

--- mysite/web_flask.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask,render_template
-app = Flask(__name__)
-
-@app.route("/show/info")
-def index():
-    return render_template("web_flask.html")
-
-if __name__ == "__main__":
-    app.run()
